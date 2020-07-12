3ac0cda24cb441b5de73253e2052b32a3d6e29d9
AbbasSAFAROV <abbasabdullaziz@gmail.com>
2020-07-12 12:24:00 +0300

    make bro

--- proje/settings.py
+++ proje/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*#pb9ftvlbpanvxx531xk2)2alu6=3i^!#uoc_^7pc!!8i$=37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
